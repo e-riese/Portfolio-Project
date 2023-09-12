62f9bfa1507bf7c27499ce7db3811c5ecf1a2885
e-riese <60417188+e-riese@users.noreply.github.com>
2023-09-12 15:00:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -11,3 +11,24 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+var my_element = document.getElementById("my_element");
+my_element.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest"
+  });
+
+
+
+  function CopyMe(TextToCopy) {
+    var TempText = document.createElement("input");
+    TempText.value = TextToCopy;
+    document.body.appendChild(TempText);
+    TempText.select();
+    
+    document.execCommand("copy");
+    document.body.removeChild(TempText);
+    
+    alert("Copied the text to clipboard: " + TempText.value);
+  }
